968a8a9065923f6d55a44324092e2decfe9f7961
Meedo365 <83066257+Meedo365@users.noreply.github.com>
2023-01-24 13:16:38 +0100

    payment updated

--- routes/endpoints/Packages.js
+++ routes/endpoints/Packages.js
@@ -1,5 +1,6 @@
 const Package = require('../../models/packages');
 const { auth } = require("../middlewares/loggedIn");
+const User = require("../../models/user");
 
 let routes = (app) => {
     app.post('/package', async (req, res) => {
@@ -13,15 +14,19 @@ let routes = (app) => {
         }
     });
 
-    app.post('/package/user', auth, async (req, res) => {
+    app.post('/package/user', async (req, res) => {
         try {
+            const { total } = req.body;
+            // const user = await User.findOne({ _id: user_id });
+            // if (!user) return res.status(400).json({ msg: "you must to login" })
             let package = new Package(req.body);
             package.status = "pending";
+            package.balance = Number(total).toLocaleString();
             await package.save()
             res.json(package)
         }
         catch (err) {
-            res.status(500).send(err)
+            res.status(500).send(err.message)
         }
     });
 
@@ -29,7 +34,8 @@ let routes = (app) => {
     app.get('/packages', async (req, res) => {
         try {
             let packages = await Package.find().sort({ createdAt: -1 })
-                .populate("product_id", "itemName price")
+                .populate("product_id.item")
+                .populate("package_category")
                 .populate("user_id", "firstname lastname role")
             res.json(packages)
         }
@@ -52,9 +58,89 @@ let routes = (app) => {
 
     app.get('/package/user/:id', async (req, res) => {
         try {
+            let arr = [];
+            let duration;
             let packages = await Package.find({ user_id: req.params.id })
                 .populate("product_id.item", "itemName price")
-            res.json(packages)
+            packages.map((e, i) => {
+                duration = e.duration
+                e.product_id.map((a, b) => {
+                    arr.push(+a.item.price.split(",").join(""))
+                    return arr.reduce((a, b) => a + b, 0)
+                })
+            })
+            let total = arr.reduce((a, b) => a + b, 0);
+            let balance = packages.balance.split(",").join("")
+            let daily = balance / (duration * 31)
+            let weekly = balance / (duration * 4)
+            let monthly = balance / (duration)
+            await Package.updateOne({ user_id: req.params.id }, {
+                total: total.toLocaleString(),
+                daily: Math.ceil(daily).toLocaleString(),
+                weekly: Math.ceil(weekly).toLocaleString(),
+                monthly: Math.ceil(monthly).toLocaleString()
+            },
+                { returnOriginal: false })
+            let packagess = await Package.find({ user_id: req.params.id, status: "pending" })
+                .populate("product_id.item", "itemName price")
+            res.json(packagess)
+        }
+        catch (err) {
+            res.status(500).send(err)
+        }
+    });
+
+    app.put('/package/user/:id', async (req, res) => {
+        try {
+            let update = req.body;
+            let package = await Package.updateOne({ _id: req.params.id }, update, { returnOriginal: false });
+            return res.json(package)
+        }
+        catch (err) {
+            res.status(500).send(err)
+            throw err
+        }
+    });
+
+    app.put('/cart/user/:id', async (req, res) => {
+        try {
+            // let { status } = req.body;
+            let package = await Package.updateOne({ _id: req.params.id }, { status: "cart" }, { returnOriginal: false });
+            return res.json(package)
+        }
+        catch (err) {
+            res.status(500).send(err)
+            throw err
+        }
+    });
+
+    app.get('/cart/user/:id', async (req, res) => {
+        try {
+            let arr = [];
+            let duration;
+            let packages = await Package.find({ user_id: req.params.id })
+                .populate("product_id.item", "itemName price")
+            packages.map((e, i) => {
+                duration = e.duration
+                e.product_id.map((a, b) => {
+                    arr.push(+a.item.price.split(",").join(""))
+                    return arr.reduce((a, b) => a + b, 0)
+                })
+            })
+            let total = arr.reduce((a, b) => a + b, 0);
+            let daily = total / (duration * 31)
+            let weekly = total / (duration * 4)
+            let monthly = total / (duration)
+            await Package.updateOne({ user_id: req.params.id }, {
+                total: total.toLocaleString(),
+                daily: Math.ceil(daily).toLocaleString(),
+                weekly: Math.ceil(weekly).toLocaleString(),
+                monthly: Math.ceil(monthly).toLocaleString(),
+            },
+                { returnOriginal: false })
+            let packagess = await Package.find({ user_id: req.params.id, status: "cart" })
+                .populate("product_id.item", "itemName price")
+            res.json(packagess)
         }
         catch (err) {
             res.status(500).send(err)
