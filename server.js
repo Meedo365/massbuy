2decfe7d4e2ba27cec5f51e22446bb1154f7be3c
Meedo365 <nuhuahmed365@gmail.com>
2023-01-25 10:48:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -29,6 +29,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyparser.urlencoded({ limit: '50mb', extended: false }));
 app.use(bodyparser.json());
 // app.use(bodyparser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'x-www-form-urlencoded, Origin, X-Requested-With, Content-Type, Accept, Authorization, *');
+    if (req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE, OPTIONS');
+        res.setHeader('Access-Control-Allow-Credentials', true);
+        return res.status(200).json({});
+    }
+    next();
+});
+
 app.use('*', cloudinaryConfig);
 
 app.get('/', (req, res) => {
