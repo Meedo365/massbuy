c7d719ef87e5c01d4117b23683ad1c3b0f0c722c
Meedo365 <83066257+Meedo365@users.noreply.github.com>
2023-02-04 10:11:24 +0100

    updated

--- server.js
+++ server.js
@@ -20,28 +20,18 @@ mongoose.connect(CONNECTION_STRING, {
 });
 
 mongoose.connection.on('open', () => console.log('Mongo Running'));
-mongoose.connection.on('error', (err) => console.log(err)); app.use(express.json());
+mongoose.connection.on('error', (err) => console.log(err));
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(cors());
 app.use(routes);
-// app.use(dotenv.config);
 app.use('/uploads', express.static('uploads'));
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use(bodyparser.urlencoded({ limit: '50mb', extended: false }));
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: true }));
-
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', 'x-www-form-urlencoded, Origin, X-Requested-With, Content-Type, Accept, Authorization, *');
-//     if (req.method === 'OPTIONS'){
-//         res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE, OPTIONS');
-//         res.setHeader('Access-Control-Allow-Credentials', true);
-//         return res.status(200).json({});
-//     }
-//     next();
-// });
-
+// app.use(bodyparser.json());
+// app.use(bodyparser.urlencoded({ extended: true }));
 app.use('*', cloudinaryConfig);
 
 app.get('/', (req, res) => {
